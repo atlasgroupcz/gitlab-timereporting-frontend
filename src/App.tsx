388b6088be757f23ae0d1d9b70e706057bec6ab1
filipkastovsky <devfilipkastovsky@gmail.com>
2020-07-17 20:45:58 +0200

    styled-components; config; cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import { AtlanticProvider, theme } from 'react-atlantic';
+import { ThemeProvider } from 'styled-components';
+import { Index } from './pages';
 export const App = () => {
-    return <></>;
+    return (
+        <ThemeProvider theme={theme}>
+            <AtlanticProvider theme={theme}>
+                <Index></Index>
+            </AtlanticProvider>
+        </ThemeProvider>
+    );
 };
---
+++ src/utils/styled.ts
@@ -0,0 +1,4 @@
+import { Theme } from 'react-atlantic';
+import styledStyled, { ThemedStyledInterface } from 'styled-components';
+
+export const styled = styledStyled as ThemedStyledInterface<Theme>;
