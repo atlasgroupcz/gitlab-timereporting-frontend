f5b35eba7ef254203cccb021ff146ca073a80ff1
filipkastovsky <devfilipkastovsky@gmail.com>
2020-07-20 01:44:10 +0200

    Rewrite useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,49 +1,57 @@
 import { useCallback, useEffect, useState } from 'react';
 import { DEFAULT_INIT, ROOT } from '../Config';
 
+type DataOrErrorsType<T, E> = {
+    data?: T | undefined;
+    errors?: E | undefined;
+};
+
+type ResultType<T, E> = DataOrErrorsType<T, E> & {
+    loading: boolean;
+};
+
+type FetchDataType<T, E> = (
+    inputOverride?: RequestInfo
+) => Promise<DataOrErrorsType<T, E>>;
+
+type UseFetchReturnType<T, E> = [FetchDataType<T, E>, ResultType<T, E>];
+
 export const useFetch = <T = any, E = any>(
     input: RequestInfo,
-    init: RequestInit = DEFAULT_INIT
-): {
-    errors: E | undefined;
-    data?: T | undefined;
-    loading: boolean;
-    fetchData: () => void;
-} => {
+    init: RequestInit = DEFAULT_INIT,
+    autoFetch = true
+): UseFetchReturnType<T, E> => {
     const [loading, setLoading] = useState<boolean>(false);
     const [data, setData] = useState<T | undefined>();
     const [errors, setErrors] = useState<E | undefined>();
 
     const reset = useCallback(() => {
-        setLoading(false);
         setErrors(undefined);
     }, []);
 
-    const fetchData = useCallback(
+    const fetchData: FetchDataType<T, E> = useCallback(
         async (inputOverride: RequestInfo = input) => {
+            reset();
             setLoading(true);
             try {
                 const res = await fetch(`${ROOT}${inputOverride}`, init);
-
                 const newData = await res.json();
-
                 setData(newData);
+                setLoading(false);
+                return { data: newData };
             } catch (error) {
                 console.error(error);
-
                 setErrors(error);
-                setData(undefined);
-            } finally {
                 setLoading(false);
+                return { errors: error };
             }
         },
-        [input, init]
+        [input, reset, init]
     );
 
     useEffect(() => {
-        reset();
-        fetchData();
-    }, [reset, fetchData]);
+        autoFetch && fetchData();
+    }, [fetchData, autoFetch]);
 
-    return { loading, data, errors, fetchData };
+    return [fetchData, { loading, data, errors }];
 };
