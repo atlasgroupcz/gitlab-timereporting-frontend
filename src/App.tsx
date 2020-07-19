330df9c2298a711fb627bd37ab5f9f1dd52ed94b
filipkastovsky <devfilipkastovsky@gmail.com>
2020-07-20 01:41:10 +0200

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,32 @@ import React, { FC } from 'react';
 
 import { AtlanticProvider, theme } from 'react-atlantic';
 import { ThemeProvider } from 'styled-components';
-import { Index } from './pages';
+
 import { ValidationSchemasProvider } from './context/ValidationSchemas';
+import { CalendarReport } from './pages/CalendarReport';
+import { Router, RouteComponentProps } from '@reach/router';
+import { Home } from './pages/Home';
+import { ExcelReport } from './pages/ExcelReport';
+import { HierarchyReport } from './pages/HierarchyReport';
+
+const HomeRoute = (_props: RouteComponentProps) => <Home />;
+const CalendarReportRoute = (_props: RouteComponentProps) => <CalendarReport />;
+const ExcelReportRoute = (_props: RouteComponentProps) => <ExcelReport />;
+const HierarchyReportRoute = (_props: RouteComponentProps) => (
+    <HierarchyReport />
+);
 
 export const App: FC = () => {
     return (
         <ValidationSchemasProvider>
             <ThemeProvider theme={theme}>
                 <AtlanticProvider theme={theme}>
-                    <Index></Index>
+                    <Router>
+                        <HomeRoute path="/" />
+                        <ExcelReportRoute path="excel" />
+                        <CalendarReportRoute path="calendar" />
+                        <HierarchyReportRoute path="calendar" />
+                    </Router>
                 </AtlanticProvider>
             </ThemeProvider>
         </ValidationSchemasProvider>
