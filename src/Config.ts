ee5b657aaeacd27d9b91566e5dbfb013766c4c01
filipkastovsky <devfilipkastovsky@gmail.com>
2020-07-20 01:41:20 +0200

    Api date format

--- src/Config.ts
+++ src/Config.ts
@@ -2,3 +2,4 @@ export const ROOT = 'http://gitlab-timereporting.k8s.atlascon.cz';
 export const DEFAULT_INIT: RequestInit = {
     headers: { 'Content-Type': 'application/json' },
 };
+export const API_DATE_FORMAT = 'YYYY-MM-DD';
